7d1a10a6d99589c999e475d2ff11553d37b9a3ee
g1n0st <g1n0st@live.com>
2023-04-12 00:22:11 +0800

    fix mass-spring benchmark

--- run_ms.py
+++ run_ms.py
@@ -15,9 +15,9 @@ args = parser.parse_args()
 ti.init(arch=getattr(ti, args.arch))
 
 DT = 5e-5
-NP = 64
-stiffness1 = 1e4
-stiffness2 = 1e4
+NP = 128
+stiffness1 = 1e7
+stiffness2 = 1e7
 eps = 1e-6
 
 x0_p = ti.Vector.field(2, float, shape=NP)
@@ -28,18 +28,20 @@ def init_p(simulator : ti.template()):
     xy = ti.Vector([0.2 + i / NP * 0.6, 0.5])
     simulator.x_p[i] = xy
     x0_p[i] = xy
-    simulator.v_p[i] = [0.0, -20.0]
+    simulator.v_p[i] = [0.0, -10.0]
 
     simulator.F_p[i] = ti.Matrix.identity(ti.f32, 2)
-    simulator.m_p[i] = simulator.p_mass
+    simulator.m_p[i] = simulator.p_mass * 0.1
 
     simulator.g_p[i] = -1
     simulator.c_p[i] = ti.Vector([0.23, 0.33, 0.93])
 
-sdf = HandlerSDF(2, np.array([[0.2, 0.5], [0.8, 0.5]], dtype=np.float32), sphere_radius = 0.1)
+sdf = HandlerSDF(2, np.array([[0.2, 0.5], [0.8, 0.5]], dtype=np.float32), sphere_radius = 0.05)
 
 @ti.kernel
 def mass_spring(solver : ti.template()):
+  for i in range(NP):
+      solver.f_p[i].fill(0.0)
   # stretch
   for i in range(solver.n_particles - 1):
     disp = solver.x_p[i] - solver.x_p[i+1]
@@ -56,15 +58,27 @@ def mass_spring(solver : ti.template()):
     solver.f_p[i] += spring_force
     solver.f_p[i+2] -= spring_force
 
+@ti.func
+def _2v2(simulator, I):
+  sz = simulator.finest_size
+  L = -1
+  if I[0] < sz / 2:
+    L = 0
+    simulator.activate_cell(0, I)
+  else: 
+    L = 1
+    simulator.activate_cell(1, I)
+  return L
+
 
 simulator = AdaptiveGIMP(dim = 2, 
-                         level = 1,
+                         level = 2,
                          sdf = sdf,
                          lag_force = mass_spring,
-                         coarsest_size = 32, 
+                         coarsest_size = 64, 
                          n_particles = NP, 
                          particle_initializer = init_p, 
-                         grid_initializer = initialize_mask1)
+                         grid_initializer = _2v2)
 gui = GUI()
 
 while True:
